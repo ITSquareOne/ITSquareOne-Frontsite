0ff03f634281f52bb0a34489bc94ee1736de1a92
jazzpu <144272963+jazzpu@users.noreply.github.com>
2025-02-27 17:56:17 +0700

    adding password show toggle to sign in

--- itsquare-app/src/app/sign-in/page.tsx
+++ itsquare-app/src/app/sign-in/page.tsx
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 
 export default function Sign_in() {
   const [isOpen, setIsOpen] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   useEffect(() => {
     // Prevent scrolling when the page loads
     document.body.style.overflow = "hidden";
@@ -21,8 +22,33 @@ export default function Sign_in() {
           <h1 className="text-4xl font-semibold text-[#190832] mt-8 mb-4 text-center">Sign in</h1>
           <label className="">Username</label>
           <input type="text" className="border-[#353535] border-2 rounded-md py-3 px-4" placeholder="Enter username"></input>
-          <label className="mt-4">Password</label>
-          <input type="password" className="border-[#353535] border-2 rounded-md py-3 px-4" placeholder="Enter password"></input>
+   
+          {/* PASSWORD INPUT WITH TOGGLE ICON */}
+        <label className="mt-4">Password</label>
+        <div className="relative w-full">
+          <input
+            type={showPassword ? "text" : "password"}
+            className="border-[#353535] border-2 rounded-md py-3 px-4 w-full pr-10"
+            placeholder="Enter password"
+          />
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute inset-y-0 right-3 flex items-center text-gray-500"
+          >
+            {showPassword ? (
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            </svg>
+            ) : (
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                </svg>
+            )}
+          </button>
+        </div>
+          
           <a onClick={() => setIsOpen(true)} className="cursor-pointer underline mt-2">Forgot Password?</a>
           <button className="mt-4 bg-gray-800 hover:bg-black text-white w-40 text-center mx-auto rounded-md py-2 hover:text-white transition delay-180 duration-300 ease-in-out">Sign in</button>
           <a href="sign-up" className="cursor-pointer underline text-center mt-4">Don't have an account?</a>
