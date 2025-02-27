2db4949a6d4f17bd0cc79dc36c6aaa6c010bcd6d
jazzpu <144272963+jazzpu@users.noreply.github.com>
2025-02-27 16:16:26 +0700

    Adding show password toggle in sign up

--- itsquare-app/src/app/sign-up/page.tsx
+++ itsquare-app/src/app/sign-up/page.tsx
@@ -1,41 +1,91 @@
 "use client";
-import Image from "next/image";
-import { useEffect } from "react";
-import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
+import { useState, useEffect } from "react";
 
 export default function Sign_up() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-  
   useEffect(() => {
-    // Prevent scrolling when the page loads
     document.body.style.overflow = "hidden";
     return () => {
-      document.body.style.overflow = "auto"; // Restore scroll when navigating away
+      document.body.style.overflow = "auto";
     };
   }, []);
+
   return (
-    <div className="relative min-h-screen bg-cover bg-center justify-center items-center flex pt-10" style={{backgroundImage: "url('/bg-main.png')"}}>
-        <div className="bg-white md:max-w-[600px] md:min-h-[400px] mb-[200px] max-w-[340px] min-h-[400px] shadow-[0px_8px_7px_1px_rgba(0,_0,_0,_0.6)]
- rounded-xl container text-black  flex flex-col px-8 pb-8 ">
-          <h1 className="text-4xl font-semibold text-[#190832] mt-8 mb-4 text-center">Sign up</h1>
-          <form action="">
-              <label className="">Username</label> <br />
-              <input type="text" className="border-[#353535] border-2 rounded-md py-3 px-4 w-full" placeholder="Enter username"></input><br />
-<br />
-              <label className="">Email</label> <br />
-              <input type="email" className="border-[#353535] border-2 rounded-md py-3 px-4 w-full" placeholder="Enter email"></input><br />
-<br />
-              <label className="mt-4">Password</label> <br />
-              <input type="password" className="border-[#353535] border-2 rounded-md py-3 px-4 w-full" placeholder="Enter password" ></input> <br />
-<br />
-              <label className="mt-4">Confirm Password</label><br />
-              <input type="password" className="border-[#353535] border-2 rounded-md py-3 px-4 w-full" placeholder="Confirm password"></input> <br />
-<br />
-              <div className="flex">
-                <button type="submit" className="mt-4 bg-gray-800 hover:bg-black text-white w-40 text-center mx-auto rounded-md py-2 hover:text-white transition delay-180 duration-300 ease-in-out">Sign up</button>
-              </div>
-          </form>
-        </div>
+    <div className="relative min-h-screen bg-cover bg-center flex justify-center items-center"
+      style={{ backgroundImage: "url('/bg-main.png')" }}>
+      <div className="bg-white md:max-w-[600px] min-h-[400px] shadow-lg rounded-xl text-black flex flex-col px-8 pb-8 w-full max-w-md -mt-20">
+        <h1 className="text-4xl font-semibold text-[#190832] mt-4 mb-4 text-center">Sign up</h1>
+        <form className="flex flex-col w-full">
+          <label>Username</label>
+          <input required type="text" className="border-[#353535] border-2 rounded-md py-3 px-4 w-full" placeholder="Enter username" />
+
+          <label className="mt-4">Email</label>
+          <input required type="email" className="border-[#353535] border-2 rounded-md py-3 px-4 w-full" placeholder="Enter email" />
+
+          {/* PASSWORD INPUT WITH TOGGLE ICON */}
+          <label className="mt-4">Password</label>
+          <div className="relative w-full">
+            <input required
+              type={showPassword ? "text" : "password"}
+              className="border-[#353535] border-2 rounded-md py-3 px-4 w-full pr-10"
+              placeholder="Enter password"
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute inset-y-0 right-3 flex items-center text-gray-500"
+            >
+              {showPassword ? (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                </svg>
+
+              )}
+            </button>
+          </div>
+
+          {/* CONFIRM PASSWORD INPUT WITH TOGGLE ICON */}
+          <label className="mt-4">Confirm Password</label>
+          <div className="relative w-full">
+            <input
+              type={showConfirmPassword ? "text" : "password"}
+              className="border-[#353535] border-2 rounded-md py-3 px-4 w-full pr-10"
+              placeholder="Confirm password"
+            />
+            <button
+              type="button"
+              onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+              className="absolute inset-y-0 right-3 flex items-center text-gray-500"
+            >
+              {showConfirmPassword ? (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                </svg>
+              )}
+            </button>
+          </div>
+
+          <div className="text-center ">
+            <button type="submit" className="w-1/3 mt-6 bg-gray-800 hover:bg-black text-white w-50 rounded-md py-3 transition duration-300">
+              Sign up
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
+
