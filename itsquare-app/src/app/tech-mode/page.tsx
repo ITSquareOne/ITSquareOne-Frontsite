eec4e6ec17c6286e164338fd41a60b0652660947
NettaeKrub <Undaaa12345@gmail.com>
2025-03-01 05:18:57 +0700

    starting tech-mode

--- itsquare-app/src/app/tech-mode/page.tsx
+++ itsquare-app/src/app/tech-mode/page.tsx
@@ -1,12 +1,15 @@
 "use client"
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
 
 export default function tech_mode() {
+  const menuItems = ["ทั้งหมด", "กำลังดำเนินการ", "เสร็จสิ้น", "ยกเลิก"];
+
   return (
     <div className="relative min-h-screen bg-cover bg-center px-8 items-center flex flex-col" style={{ backgroundImage: "url('/bg-main.png')" }}>
+      <div className="container flex flex-row mt-4  gap-4 flex-wrap items-center">
 
+        </div>
     </div>
   );
 }
