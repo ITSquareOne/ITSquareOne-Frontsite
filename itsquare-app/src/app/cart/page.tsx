e6e8f954628ea30a8d27f5fd42c336bd58d3db16
NettaeKrub <Undaaa12345@gmail.com>
2025-03-05 19:06:46 +0700

    fix cart ui

--- itsquare-app/src/app/cart/page.tsx
+++ itsquare-app/src/app/cart/page.tsx
@@ -31,11 +31,14 @@ export default function PcBuildSummary() {
           {/* SPECS Scrollable Section */}
           <div className="p-4 space-y-4 max-h-96 overflow-y-auto">
             {cart.map((item) => (
-                <div key={item.id}>
-                    <p>{item.name}</p>
-                    <p>{item.price} บาท</p>
-                    <p>จำนวน: {item.quantity}</p>
-                    <button onClick={() => removeFromCart(item.id)}>ลบสินค้า</button>
+                <div key={item.id} className="flex flex-row">
+                    <Image src={`${item.image}`} alt="PC Case" width={150} height={100} className="mr-4"/>
+                    <div> 
+                        <p>{item.name}</p>
+                        <p>{item.price} บาท</p>
+                        <p className="mb-2">จำนวน: {item.quantity}</p>
+                        <button className="bg-red-500 hover:bg-red-600 text-white transition p-1 px-2 shadow-md rounded-lg" onClick={() => removeFromCart(item.id)}>ลบสินค้า</button>
+                    </div>
                 </div>
             ))}
             <p>รวมทั้งหมด: {totalPrice} บาท</p>
